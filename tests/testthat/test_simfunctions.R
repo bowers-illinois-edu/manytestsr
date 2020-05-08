2d9080e680bfb2429e56ae34437763010768cd45
Jake Bowers <jake@jakebowers.org>
2020-05-07 21:21:08 -0500

    Latest package. Builds now. Check includes warnings but no errors.

--- tests/testthat/test_simfunctions.R
+++ tests/testthat/test_simfunctions.R
@@ -40,194 +40,8 @@ alpha_and_splits <- expand.grid(
 )
 alpha_and_splits$splitby <- "hwt"
 alpha_and_splits$splitby[grep("Specified", alpha_and_splits$sfn)] <- "lvs"
-
-###
-### Test the padj sim function that we use.
-## p_sims_res <- parSapplyLB(cl,1:nrow(simparms), FUN=function(i) {
-set.seed(12345)
-i <- 1
-x <- alpha_and_splits[i, ]
-xnm <- paste(x, collapse = "_")
-message(xnm)
-nsims <- 10
-
-p_sims_tab <- padj_test_fn(
-  idat = idat3,
-  bdat = bdat4,
-  blockid = "bF",
-  trtid = "Z",
-  fmla = Ynorm_inc ~ ZF | blockF,
-  ybase = "y0",
-  prop_blocks_0 = .5,
-  tau_fn = tau_norm_covariate_outliers,
-  tau_size = .5,
-  covariate = "v4",
-  pfn = pIndepDist,
-  afn = getFromNamespace(x[["afn"]], ns = "manytests"),
-  nsims = nsims,
-  ncores = 1, ## parallelize over the  rows of simparms
-  reveal_and_test_fn = reveal_po_and_test_siup,
-  splitfn = getFromNamespace(x[["sfn"]], ns = "manytests"),
-  splitby = x[["splitby"]]
-)
 oneway_test(Y_zeros ~ ZF | bF, data = idat)
 ####
-
-
-## Half blocks null
-tausb_zeros <- idat[, .(truetau_zeros = mean(y1test_zeros - y0)), by = bF]
-tausb_zeros
-bdat_zeros <- merge(bdat, tausb_zeros)
-
-siures_zeros <- findBlocks(
-  idat = idat, bdat = bdat_zeros, blockid = "bF",
-  pfn = pIndepDist, splitfn = splitEqualApprox, thealpha = .05, splitby = "hwt",
-  fmla = Y_zeros ~ ZF | bF, parallel = "multicore", copydts = TRUE
-)
-setkey(siures_zeros, bF)
-siures_zeros
-
-siufinal_zeros <- siures_zeros[, .(
-  p = unique(pfinalb),
-  truetau = mean(truetau_zeros), nb = .N,
-  blocks = paste(bF, collapse = ",")
-), by = biggrp]
-siufinal_zeros
-
-siures_zeros_g <- make_graph(make_tree(siures_zeros))
-
-
-# table(siufinal_zeros$p <= .05, siufinal_zeros$truetau > 0, exclude = c())
-siures_zeros_det <- report_detections(siures_zeros, only_hits = FALSE)
-setkey(siures_zeros_det, bF)
-setkey(siures_zeros, bF)
-zeros_det <- siures_zeros[siures_zeros_det][, .(hit_grp, hit, max_p, fin_parent_p, bF, biggrp, truetau_zeros)][order(truetau_zeros), ]
-zeros_det
-table(zeros_det$hit_grp, noeffects = zeros_det$truetau_zeros == 0, exclude = c())
-
-### Now for all null --- but actually not all are exactly null
-tausb_null <- idat[, .(truetau_null = mean(y1test_null - y0)), by = bF]
-tausb_null
-bdat_null <- merge(bdat, tausb_null)
-siures_null <- findBlocks(
-  idat = idat, bdat = bdat_null, blockid = "bF",
-  pfn = pIndepDist, splitfn = splitLOO, thealpha = .05,
-  fmla = Y_null ~ ZF | bF, parallel = "multicore", copydts = TRUE
-)
-setkey(siures_null, bF)
-siures_null
-siufinal_null <- siures_null[, .(
-  p = unique(pfinalb),
-  truetau = mean(truetau_null), nb = .N,
-  blocks = paste(bF, collapse = ",")
-), by = biggrp]
-siufinal_null
-# table(siufinal_null$p <= .05, abs(siufinal_null$truetau) > 0, exclude = c())
-
-siures_null_g <- make_graph(make_tree(siures_null))
-
-## What does report_detections give when we detect only at the first stage? Ans: One hit_grp exactly as desired.
-
-null_det <- report_detections(siures_null, only_hits = FALSE)
-setkey(null_det, bF)
-setkey(siures_null, bF)
-null_det <- siures_null[null_det][, .(hit_grp, hit, max_p, fin_parent_p, bF, biggrp, truetau_null)][order(truetau_null), ]
-null_det
-table(null_det$hit_grp, noeffects = null_det$truetau_null == 0, exclude = c())
-
-#### Working STARTING HERE. Eventually this will all be wrapped in test_that with expectations etc..
-## Doing it with 20 blocks to better quality assess the calculations of false positive rates, and other error rates:
-# tausb_norm_inc <- idat3[, .(truetau_norm_inc = mean(y1norm_inc - y0)), by = bF]
-# tausb_norm_inc
-# bdat4_norm_inc <- merge(bdat4, tausb_norm_inc) ## this is just to connect with above, we already have ate_norm_inc as our true mean effect
-siures_norm_inc <- findBlocks(
-  idat = idat3, bdat = bdat4, blockid = "bF",
-  pfn = pIndepDist, splitfn = splitCluster, thealpha = .05, splitby = "hwt", alphafn = alpha_investing,
-  fmla = Ynorm_inc ~ ZF | bF, parallel = "multicore", copydts = TRUE, blocksize = "hwt"
-)
-setkey(siures_norm_inc, bF)
-siures_norm_inc
-
-
-## biggrp records leaves
-siufinal_norm_inc <- siures_norm_inc[, .(
-  p = unique(pfinalb),
-  truetau = mean(ate_norm_inc), nb = .N,
-  blocks = paste(bF, collapse = ",")
-), by = biggrp]
-siufinal_norm_inc
-# table(siufinal_norm_inc$p <= .05, abs(siufinal_norm_inc$truetau) > 0, exclude = c())
-
-siures_norm_inc_g <- make_graph(make_tree(siures_norm_inc))
-
-norm_inc_det <- report_detections(siures_norm_inc, fwer = FALSE, only_hits = FALSE)
-norm_inc_det[, .(
-  hit_grp, hit, bF, biggrp, nodenum_current, max_p,
-  fin_parent_p, ate_norm_inc, max_alpha, parent_alpha
-)][order(ate_norm_inc), ]
-
-## These are the tests:
-norm_inc_det_fin_nodes <- norm_inc_det[, .(
-  hit = as.numeric(unique(hit)),
-  nodenum = paste(unique(nodenum_current), collapse = ","),
-  blocks = paste(unique(bF), collapse = ","),
-  nbiggrp = length(unique(biggrp)), # stri_count(biggrp,fixed=",")
-  nblocks = length(unique(bF)),
-  numnull = sum(ate_norm_inc == 0),
-  anynull = as.numeric(any(ate_norm_inc == 0)),
-  allnull = as.numeric(all(ate_norm_inc == 0)),
-  numnotnull = sum(ate_norm_inc != 0),
-  anynotnull = as.numeric(any(ate_norm_inc != 0)),
-  mintau = min(ate_norm_inc),
-  mntau = mean(ate_norm_inc),
-  maxtau = max(ate_norm_inc),
-  minnb = min(nb),
-  meannb = mean(nb),
-  maxnb = max(nb),
-  maxalpha = max(max_alpha),
-  minalpha = min(max_alpha)
-), by = hit_grp][order(-hit, anynotnull), ]
-norm_inc_det_fin_nodes
-
-norm_inc_det_fin_errs <- norm_inc_det_fin_nodes[, .(
-  nreject = sum(hit),
-  naccept = sum(1 - hit),
-  prop_reject = mean(hit),
-  prop_accept = mean(1 - hit), # or 1-prop_reject
-  ## rejections of false null /detections of true non-null
-  ## (if any of the blocks have an effect, then we count this as a correct rejection or correct detection)
-  true_pos_prop = mean(hit * anynotnull),
-  ## If we reject but *all* of the blocks have no effects, this is a false positive error
-  ## If we reject but only one of them has no effects but the other has effects,
-  ## then this is not an error --- but a correct detection
-  false_pos_prop = mean(hit * allnull),
-  ## If we do not reject and all blocks are truly null, then we have no error.
-  true_neg_prop = mean((1 - hit) * allnull),
-  ## If we do not reject/detect and at least one of the blocks actually has an effect, we have
-  ## a false negative error --- a failure to detect the truth
-  false_neg_prop = mean((1 - hit) * anynotnull),
-  ## Now look at false and true discoveries: false rejections as a proportion of rejections
-  false_disc_prop = sum(hit * allnull) / max(1, sum(hit)),
-  true_disc_prop = sum(hit * anynotnull) / max(1, sum(hit)),
-  true_nondisc_prop = sum((1 - hit) * allnull) / max(1, sum(1 - hit)),
-  false_nondisc_prop = sum((1 - hit) * anynotnull) / max(1, sum(1 - hit))
-)]
-norm_inc_det_fin_errs
-
-## if allnull==FALSE then anynotnull=TRUE
-
-err_tab1 <- with(norm_inc_det_fin_nodes, table(hit, anynotnull, exclude = c()))
-err_tab2 <- with(norm_inc_det_fin_nodes, table(hit, allnull, exclude = c()))
-
-stopifnot(with(norm_inc_det_fin_errs, true_pos_prop == nreject / (nreject + naccept)))
-stopifnot(with(norm_inc_det_fin_errs, true_pos_prop == err_tab2["1", "0"] / sum(err_tab2)))
-stopifnot(with(norm_inc_det_fin_errs, true_neg_prop == err_tab2["0", "1"] / sum(err_tab2)))
-stopifnot(with(norm_inc_det_fin_errs, false_pos_prop == err_tab2["1", "1"] / sum(err_tab2)))
-stopifnot(with(norm_inc_det_fin_errs, false_neg_prop == err_tab2["0", "0"] / sum(err_tab2)))
-stopifnot(with(norm_inc_det_fin_errs, true_disc_prop == err_tab2["1", "0"] / max(1, sum(err_tab2["1", ]))))
-stopifnot(with(norm_inc_det_fin_errs, false_disc_prop == err_tab2["1", "1"] / max(1, sum(err_tab2["1", ]))))
-stopifnot(with(norm_inc_det_fin_errs, true_nondisc_prop == err_tab2["0", "1"] / max(1, sum(err_tab2["0", ]))))
-stopifnot(with(norm_inc_det_fin_errs, false_nondisc_prop == err_tab2["0", "0"] / max(1, sum(err_tab2["0", ]))))
 
 ## Now using the function:
 
@@ -456,6 +270,36 @@ test_that("Error calculations for a given set of tests work: Testing in every bl
   )
 })
 
+
+###
+### Test the padj sim function that we use.
+## p_sims_res <- parSapplyLB(cl,1:nrow(simparms), FUN=function(i) {
+set.seed(12345)
+i <- 1
+x <- alpha_and_splits[i, ]
+xnm <- paste(x, collapse = "_")
+message(xnm)
+nsims <- 10
+
+p_sims_tab <- padj_test_fn(
+  idat = idat3,
+  bdat = bdat4,
+  blockid = "bF",
+  trtid = "Z",
+  fmla = Ynorm_inc ~ ZF | blockF,
+  ybase = "y0",
+  prop_blocks_0 = .5,
+  tau_fn = tau_norm_covariate_outliers,
+  tau_size = .5,
+  covariate = "v4",
+  pfn = pIndepDist,
+  afn = getFromNamespace(x[["afn"]], ns = "manytestsr"),
+  nsims = nsims,
+  ncores = 1, ## parallelize over the  rows of simparms
+  reveal_and_test_fn = reveal_po_and_test_siup,
+  splitfn = getFromNamespace(x[["sfn"]], ns = "manytestsr"),
+  splitby = x[["splitby"]]
+)
 
 ## Testing the error simulation function (deprecated I think. Not used anymore)
 ## set.seed(12345)
