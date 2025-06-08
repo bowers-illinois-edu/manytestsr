c8be24cb4a1e54e373de4010a3662f28afddaace
Jake Bowers <jake@jakebowers.org>
2025-06-08 14:23:59 -0500

    Update a p-value function that just uses means and ranks

--- R/pval_fns.R
+++ R/pval_fns.R
@@ -207,3 +207,100 @@ pIndepDist <- function(dat, fmla = YcontNorm ~ trtF | blockF, simthresh = 20, si
   }
   return(as.numeric(thep))
 }
+
+#' P-value function: Testing twice
+#'
+#' @description
+#' These functions accept a data frame and perhaps test specific arguments
+#' (like whether or not the test will be asymptotic or simulation based). It
+#' produces a p-value.
+#'
+#' @details
+#' For now, this  function  does an omnibus-style max-T test using (1) the raw outcome and (2) a rank transformed raw outcome.
+#'
+#' @param dat An object inheriting from class data.frame
+#' @param fmla  A formula  appropriate to the function. Here it should  be something like outcome~treatment|block
+#' @param sims Either NULL (meaning use an asymptotic reference dist) or a
+#' number (meaning sampling from the randomization distribution implied by the
+#' formula)
+#' @param simthresh is the size of the data below which we use direct permutations for p-values
+#' @param groups is a vector defining the groups within which the inter-unit  distances are calculated. Not used here.
+#' @param distfn is  a function that produces one or more vectors (a data frame or matrix) of the  same number of  rows as the dat
+#' @param parallel is "no" then parallelization is not required, otherwise it is "multicore" or "snow" in the call to `coin::independence_test()` (see  help for coin::approximate()). Also, if parallel is not "no" and `adaptive_dist_function` is TRUE, then an openmp version of the distance creation function is called using `ncpu` threads (or `parallel::detectCores(logical=FALSE)` cores).
+#' @param ncpu is number of cpus  to be used for parallel operation.
+#' @param adaptive_dist_function is TRUE if the distance calculation function should be chosen using previous benchmarks. See the code.
+#' @return A p-value
+#' @importFrom coin independence_test pvalue approximate exact asymptotic
+#' @importFrom Rfast Rank
+#' @importFrom parallel detectCores
+#' @importFrom dataPreparation which_are_constant
+#' @export
+pTestTwice <- function(dat, fmla = YcontNorm ~ trtF | blockF, simthresh = 20, sims = 1000,
+                       parallel = "yes", ncpu = NULL, groups = NULL) {
+  fmla_vars <- all.vars(fmla)
+  theresponse <- fmla_vars[attr(terms(fmla), "response")]
+  # if we get a constant outcome, then p=1.
+  # no evidence against the null of no effects.
+  if (length(unique(dat[[theresponse]])) < 2) {
+    return(1)
+  }
+
+  dat_size <- nrow(dat)
+
+  if (is.null(ncpu) && parallel != "no") {
+    ## This is used for both distfn but also for independence_test for it to do
+    ## permutation testing in parallel in small blocks/samples
+
+    ncpu <- detectCores()
+  }
+
+  thetreat <- fmla_vars[[2]]
+  thedat <- copy(dat)
+  outcome_names <- c(theresponse, "rankY")
+  if (length(fmla_vars) == 3) {
+    theblock <- fmla_vars[[3]]
+    thedat[, outcome_names[-1] := frank(get(theresponse)), by = get(theblock)]
+    # If one of the test statistics is constant, drop it.
+    # https://stackoverflow.com/questions/15068981/removal-of-constant-columns-in-r
+    anyconstant_cols <- dataPreparation::which_are_constant(thedat[, .SD, .SDcols = outcome_names], verbose = FALSE)
+    if (length(anyconstant_cols) > 0) {
+      outcome_names <- outcome_names[-anyconstant_cols]
+    }
+    newfmla_text <- paste(paste(outcome_names, collapse = "+"), "~", thetreat, "|", theblock, sep = "")
+  } else {
+    theblock <- NULL
+    # This next is faster than doing it in two lines
+    thedat[, outcome_names[-1] := frank(get(theresponse))]
+    # If one of the test statistics is constant, drop it.
+    # https://stackoverflow.com/questions/15068981/removal-of-constant-columns-in-r
+    anyconstant_cols <- dataPreparation::which_are_constant(thedat[, .SD, .SDcols = outcome_names], verbose = FALSE)
+    if (length(anyconstant_cols) > 0) {
+      outcome_names <- outcome_names[-anyconstant_cols]
+    }
+    newfmla_text <- paste(paste(outcome_names, collapse = "+"), "~", thetreat, sep = "")
+  }
+  newfmla <- as.formula(newfmla_text)
+  if (is.null(simthresh) | nrow(thedat) > simthresh) {
+    suppressWarnings(
+      # thep <- pvalue(independence_test(newfmla, data = thedat, teststat = "maximum", alternative="greater"))[[1]]
+      thep <- pvalue(independence_test(newfmla, data = thedat, teststat = "quadratic"))[[1]]
+    )
+  } else {
+    if (parallel == "no") {
+      ncpu <- 1
+    }
+    suppressWarnings(
+      thep <- pvalue(independence_test(newfmla,
+        data = thedat,
+        teststat = "quadratic",
+        ## 	alternative= "greater",
+        distribution = approximate(
+          nresample = sims,
+          parallel = parallel,
+          ncpus = ncpu
+        )
+      ))[[1]]
+    )
+  }
+  return(as.numeric(thep))
+}
